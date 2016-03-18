9773dd14dea99ce890bed3f1f3fd1ba1574496a8
peter <pesegal@gmail.com>
2016-03-17 18:13:54 -0700

    Build out dynamic resizing of task window screen based  on window size and update readme document.

--- main.py
+++ main.py
@@ -1,11 +1,55 @@
 """
     Author: Peter Segal
+    Todo List Plus
 
 """
 from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+
+class TaskListViewFull(BoxLayout):
+    pass
+
+
+class TaskListViewHalf(BoxLayout):
+    pass
+
+
+class TaskListViewSingle(BoxLayout):
+    pass
+
+
+class TaskListViewController(Widget):
+    def __init__(self, **kwargs):
+        super(TaskListViewController, self).__init__(**kwargs)
+        self.bind(size=self.screen_resize)
+        self.full_view = TaskListViewFull()
+        self.half_view = TaskListViewHalf()
+        self.sing_view = TaskListViewSingle()
+
+        self.current_display = self.sing_view
+
+    def screen_resize(self, *args):
+
+        def swap_display(display):
+            self.remove_widget(self.current_display)
+            self.current_display = display
+            self.add_widget(self.current_display)
+            self.current_display.size = self.size
+
+        if args[1][0] < 640:
+            swap_display(self.sing_view)
+        elif 640 < args[1][0] < 960:
+            swap_display(self.half_view)
+        elif args[1][0] > 960:
+            swap_display(self.full_view)
+
 
 class TaskApp(App):
-    pass
+    def build(self):
+        return TaskListViewController()
+
 
 if __name__ == '__main__':
     TaskApp().run()
---
+++ readme.md
@@ -0,0 +1,20 @@
+# TaskTracker++
+
+Task tracker is the todo list app that I have always wanted. I wanted it to be simple enough that it didn't get in the way of getting stuff done. But I wanted some more advanced features that most task apps are lacking. 
+
+This includes a timer to help keep focus and statistical tracking to see how much time you dedicated to each task. See stats of what you worked on and how long tasks and projects take you.
+
+
+### TODO:
++ Build out task creation functionality
++ Build out timer window
++ Build out stats tracking functionality
+
+
+
+### Potential Future Features
++ Ability to categorize tasks into groups (epics) that will will provide statistical summary of tasks contained within.
++ Graphs and other Visualizations that can provide interesting insights where your time is spent.
++ Ability to log-in and store data in the cloud.
++ Configurable Settings
++ ect...
