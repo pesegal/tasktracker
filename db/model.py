01e4c98dc7025f4eebb3a8c77e3846348fc6d0ac
peter <pesegal@gmail.com>
2016-05-23 17:49:11 -0700

    loading sql commands from  a file

--- db/model.py
+++ db/model.py
@@ -3,11 +3,29 @@
 """
 
 import sqlite3
+from sqlite3 import OperationalError
 
 
-db = sqlite3.connect('tt_dev')
+def execute_scripts_from_file(filename):
+    fd = open(filename, 'r')
+    sql_file = fd.read()
+    fd.close()
 
+    sql_commands = sql_file.split(';')
+
+    for command in sql_commands:
+        try:
+            cursor.execute(command)
+        except OperationalError as msg:
+            print('Command Skipped: ', msg)
+
+
+db = sqlite3.connect('tt_dev.db')
 cursor = db.cursor()
 
+execute_scripts_from_file('tt_schema.sql')
 
 
+db.commit()
+
+db.close()
