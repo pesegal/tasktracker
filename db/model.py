6a95e94c51e01225e7abb98c25d765ef84d253d7
peter <pesegal@gmail.com>
2016-06-07 17:44:13 -0700

    Added default items to tables

--- db/model.py
+++ db/model.py
@@ -4,9 +4,12 @@
 
 import os
 import sqlite3
+from datetime import datetime
 from sqlite3 import OperationalError
 
 RESET_DATABASE = True
+
+print(sqlite3.sqlite_version)
 
 if RESET_DATABASE:
     os.remove('tt_dev.db')
@@ -25,11 +28,32 @@ def execute_scripts_from_file(filename):
         except OperationalError as msg:
             print('Command Skipped: ', msg)
 
-
 db = sqlite3.connect('tt_dev.db')
 cursor = db.cursor()
 
 execute_scripts_from_file('tt_schema.sql')
+# Insert Defaults
+
+columns = (
+    'Today',
+    'Tomorrow',
+    'Future',
+    'Done'
+)
+
+for data in columns:
+    cursor.execute("INSERT INTO columns(creation_date, name) VALUES (?,?)", (datetime.now(), data))
+
+action_types = (
+    'unknown',
+    'Pomodoro',
+    'Pause',
+    'Short Break',
+    'Long Break'
+)
+
+for data in action_types:
+    cursor.execute("INSERT INTO action_type(creation_date, action_description) VALUES (?,?)", (datetime.now(), data))
 
 
 db.commit()
