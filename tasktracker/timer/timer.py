7314f47820f49b9b2edaef132884ae4eeaa6bc22
peter <pesegal@gmail.com>
2016-09-06 22:14:00 -0700

    Start / Pause timer button functionality added.

--- tasktracker/timer/timer.py
+++ tasktracker/timer/timer.py
@@ -5,8 +5,9 @@
 from kivy.clock import Clock
 from kivy.uix.screenmanager import Screen
 from kivy.uix.togglebutton import ToggleButton, ToggleButtonBehavior
-from kivy.properties import NumericProperty, ObjectProperty
+from kivy.properties import NumericProperty, ObjectProperty, ListProperty, BooleanProperty
 
+from tasktracker.themes import themes
 from tasktracker.themes.themes import Themeable
 
 # TODO: Break these out to the settings module
@@ -14,23 +15,47 @@ POMOTIME = 25
 SHORT_BREAK = 5 * 60
 LONG_BREAK = 15
 
+# TODO: Create timer selection logic for buttons.
+# TODO: Create bool flag to switch between start and stop functionality.
+
 
 class TimerScreen(Screen, Themeable):
-    current_time = NumericProperty(0)
+    """ TimerScreen Object contains all of the  control logic relating to the timer screen.
+    The layout information of the Timer Screen is located in the 'timer.kv' file in the layouts
+    directory.
+    """
+    # Theme Properties
+    text_color = ListProperty(themes.THEME_CONTROLLER.text)
+
+    # Functional Properties
+    current_time = NumericProperty(SHORT_BREAK)
     _clock_event = ObjectProperty()
-    timer_type_selection = NumericProperty(0)
+    timer_active = BooleanProperty(False)
 
     def __int__(self, **kwargs):
         super().__init__(**kwargs)
-        self.bind(self.timer_type_selection, self.do_stuff)
+        self.timer_type_selection = 0
 
-    def do_stuff(self):
-        print(self.timer_type_selection)
+    def theme_update(self):
+        self.text_color = self.theme.text
+        self.text_color[3] = .88
 
-    def on_start(self):
-        self.current_time = SHORT_BREAK
-        # Check to make sure timer works at correct speed.
-        self._clock_event = Clock.schedule_interval(self.count_down, 0.016)
+    def switch_timer_type(self, selected):
+        self.timer_type_selection = selected
+        print("Timer Type Selected: ", self.timer_type_selection)
+        # TODO: Add logic here to display the selected time on the timer label.
+
+    def start_pause_trigger(self):
+        print(self.current_time)
+        # TODO: Need to call the function to make sure that default times are displayed before starting timer.
+        if self.timer_active:
+            self.ids.start_pause_button.text = 'Start'
+            Clock.unschedule(self._clock_event)
+            self.timer_active = False
+        else:
+            self._clock_event = Clock.schedule_interval(self.count_down, 0.016)
+            self.ids.start_pause_button.text = 'Pause'
+            self.timer_active = True
 
     def update(self, nap):
         self.current_time += nap
@@ -52,12 +77,8 @@ class TimerScreen(Screen, Themeable):
     def update_time(self, nap):
         pass
 
-    def theme_update(self):
-        pass
-
 
 class TimerSettingsButton(ToggleButton, Themeable):
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
