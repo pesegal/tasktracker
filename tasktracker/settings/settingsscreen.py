60172555a44f89733b42dfd8d07723860534ef9d
peter <pesegal@gmail.com>
2017-08-08 16:57:08 -0700

    dynamic loading of theme toggle buttons

--- tasktracker/settings/settingsscreen.py
+++ tasktracker/settings/settingsscreen.py
@@ -1,9 +1,11 @@
 
 from tasktracker.themes import themes
-from tasktracker.themes.themes import Themeable
+from tasktracker.themes.themes import Themeable, THEME_CONTROLLER
 
 from kivy.uix.screenmanager import Screen
+
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.uix.togglebutton import ToggleButton, ToggleButtonBehavior
 from kivy.properties import StringProperty, ListProperty
 
@@ -17,15 +19,25 @@ class SettingsScreen(Screen, Themeable):
     def __init__(self, **kwargs):
         super(Screen, self).__init__(**kwargs)
 
+    def theme_update(self):
+        pass
 
 
 class SettingsContainer(BoxLayout, Themeable):
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def theme_update(self):
         pass
+
+
+class ThemeSettingsContainer(SettingsContainer):
+    """ Dynamically adds theme selection from configuration file."""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_widget(SettingsLabel(text='Theme Selection:'))
+        for theme_name in [theme.name for theme in THEME_CONTROLLER.theme_list]:
+            self.add_widget(ThemeSelectionToggleButton(text=theme_name, group='theme_selection'))
 
 
 class SettingsToggleButton(ToggleButton, Themeable):
@@ -51,4 +63,27 @@ class SettingsToggleButton(ToggleButton, Themeable):
     def _do_press(self):
         if self.state == 'normal':
             ToggleButtonBehavior._do_press(self)
+
+
+class ThemeSelectionToggleButton(SettingsToggleButton):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def on_press(self):
+        THEME_CONTROLLER.set_theme(self.text)
+
+
+class SettingsLabel(Label, Themeable):
+    text_color = ListProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        text_c = self.theme.text
+        text_c[3] = .8
+        self.color = text_c
+
+    def theme_update(self):
+        text_c = self.theme.text
+        text_c[3] = .8
+        self.color = text_c
 
